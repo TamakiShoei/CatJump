e3da3ce2a77a08aad4a6d57761a56df0556b46f0
MSI\yoghu <t.shouei20040720@gmail.com>
2022-11-15 14:07:13 +0900

    Scene遷移関数を実装。それに伴い、分かりやすいように背景を挿入

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     private int score;
     private bool isGameFinish;
 
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     public void Initialize()
     {
         score = 0;
         isGameFinish = false;
+    }
+
+    public void ChangeScene(string next_scene)
+    {
+        SceneManager.LoadScene(next_scene);
     }
 
     public void IncrementScore(int val)
@@ -30,5 +46,25 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     public bool GetIsGameFinish()
     {
         return isGameFinish;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instance.ChangeScene("Game");
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Instance.ChangeScene("Result");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Instance.ChangeScene("Record");
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Instance.ChangeScene("Title");
+        }
     }
 }
